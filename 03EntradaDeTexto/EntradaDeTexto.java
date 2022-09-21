a75aff1b63d205ed3d0dcbe0c9c0da204e79cac7
brandonraziel12 <b.demora.r580@edu.utc.mx>
2022-09-21 08:47:52 -0500

    EntradaDeTexto

---
+++ 03EntradaDeTexto/EntradaDeTexto.java
@@ -0,0 +1,17 @@
+
+
+class EntradaTexto{
+    //metodo principal
+
+    public static void main(String[]args){
+        //estructura basica
+        //declaracion de variable
+        String nombre;
+        //mensaje de usuario
+        System.out.println( "Porfavor introduce tu nombre: ");
+        
+        //Ln es salto de linea
+        nombre = System.console().readLine();
+        System.out.println("Hola " + nombre + "Bienvenido a la tronadera");
+    }
+}
